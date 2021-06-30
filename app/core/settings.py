b69e3a41049ca0ce428a64b667150b02eaff98dd
Lucas Ferreira <lucasferreiraek@gmail.com>
2021-06-30 17:43:15 -0300

    Add jwt authentication

--- app/core/settings.py
+++ app/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication.apps.AuthenticationConfig',
     'rest_framework',
     'drf_yasg',
 ]
@@ -137,6 +138,7 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
